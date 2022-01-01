6c718f687ab205d9d9557721c973eaa27b1f2f4e
Solo-steven <ssteven19@kimo.com>
2022-01-01 22:15:57 +0800

    Reset Eslint

--- model/user.js
+++ model/user.js
@@ -1,12 +1,12 @@
-const { Schema, model } = require('mongoose');
+const {Schema, model} = require('mongoose');
 
 const userSchema = new Schema({
-    name: String,
-    imgUrl: String,
-    email: String,
-    passwordHash: String,
-    salt: String,
-    friends: [String]
+  name: String,
+  imgUrl: String,
+  email: String,
+  passwordHash: String,
+  salt: String,
+  friends: [String],
 });
 
-module.exports = model("users", userSchema);
+module.exports = model('users', userSchema);
--- router/user.js
+++ router/user.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userModel = require('../model/user');
 const config = require('../config.json');
-const JWT = require("jsonwebtoken");
+const JWT = require('jsonwebtoken');
 /**
  * @swagger
  * path:
@@ -36,30 +36,31 @@ const JWT = require("jsonwebtoken");
  *                          type: string
  */
 router.get('/', async ( req, res) => {
-    const token = req.cookies.token;
-    if(!token)
-        return res.status(401).json({message: "un-auth"});
-    try{
-        const { email } = JWT.verify(token,config.JWT );
-        const user = (await userModel.find({email: email}))[0];
-        const friends = []
-        for(const friend of user.friends)  {
-            const data = (await userModel.find({email: friend}))[0];
-            friends.push({
-                name: data.name,
-                imgUrl: data.imgUrl,
-                email: data.email,
-            })
-        }
-        return res.status(200).json({
-            name: user.name,
-            imgUrl: user.imgUrl,
-            email: user.email,
-            friends,
-        });
-    }catch(error) {
-        return res.status(401).json({message: "un-auth"});
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({message: 'un-auth'});
+  }
+  try {
+    const {email} = JWT.verify(token, config.JWT );
+    const user = (await userModel.find({email: email}))[0];
+    const friends = [];
+    for (const friend of user.friends) {
+      const data = (await userModel.find({email: friend}))[0];
+      friends.push({
+        name: data.name,
+        imgUrl: data.imgUrl,
+        email: data.email,
+      });
     }
+    return res.status(200).json({
+      name: user.name,
+      imgUrl: user.imgUrl,
+      email: user.email,
+      friends,
+    });
+  } catch (error) {
+    return res.status(401).json({message: 'un-auth'});
+  }
 });
 
 
