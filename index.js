89410fc0d3f75933cb8526755d51dd7b3ed5a504
Solo-steven <ssteven19@kimo.com>
2022-01-05 01:00:34 +0800

    Feat: Introduce Socket.io
    * Using Three event for handle chat message
       - 'init':
          Start add socket to memory map.let other
          user can send message to this socket.
       - message
          'message/input' receive message from user
          save to database first.then if receiver
          has already start socket, then send it
          by 'message/output' event

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const http = require('http');
 const express = require('express');
+const {Server} = require('socket.io');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
@@ -26,7 +28,34 @@ app.use('/user', userRouter);
 const messageRouter = require('./router/message');
 app.use('/message', messageRouter);
 
-app.listen(config.server.port, async () => {
+/**
+ *  Socket set up
+ */
+const messageModel = require('./model/message');
+const sockets = {};
+const server = http.createServer(app);
+const io = new Server(server);
+io.on('connect', (socket) => {
+  socket.on('init', async (email) => {
+    console.log('[New Socket]:', email);
+    sockets[email] = socket;
+  });
+  socket.on('message/input', async (data) => {
+    const {sender, reciver, message} = data;
+    if (!sender || !reciver|| !message) return;
+    console.log('[Socket Messgae Input]', sender, reciver, message);
+    sockets[sender] = socket;
+    const newData = new messageModel({
+      sender, content: message, reciver, timestamp: (new Date()).toISOString(),
+    });
+    newData.save();
+    if (!sockets[reciver]) {
+      return;
+    }
+    sockets[reciver].emit('message/output', {message, sender});
+  });
+});
+server.listen(config.server.port, async () => {
   const {host, port, name} = config.db;
   await mongoose.connect(`mongodb://${host}:${port}/${name}`);
   console.log('Server run', config.server.port);
